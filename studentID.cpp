a2ba4c0fd3d21f313cdb3a5c5bdbbbee2f334fd0
Aaron <91396239+ClassErased@users.noreply.github.com>
2025-09-25 17:29:20 +0100

    Update studentID.cpp

--- studentID.cpp
+++ studentID.cpp
@@ -7,18 +7,14 @@
 int main() 
 {
     
-    
     std::string input; // Intialise a str called input
     int sum = 0; // Initialise an int sum
-
 
     std::cout << "Please enter your student ID: ";
     std::cin >> input; // Assign user input to str
 
-
     /* Iterate over each character in the input
     This could also be done in the form (int i = 0; i < input.length(); i++)*/
-
 
     for (char c : input) 
     {
@@ -28,19 +24,10 @@ int main()
                 if (digit % 2 == 0) // Check if digit is divisble by two
                 {
                 sum += digit; // Add to sum
-
-
-            } else continue; 
-       
-       
+            } else continue;   
         }
-
-
     }
-
-
     std::cout << sum;
     return 0;
-
-
 }
+
