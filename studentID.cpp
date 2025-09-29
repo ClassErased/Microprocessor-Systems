b62795b8aaac83c758123cc772da74a3c8d41da9
Aaron <91396239+ClassErased@users.noreply.github.com>
2025-09-29 20:46:53 +0100

    Update studentID.cpp

--- studentID.cpp
+++ studentID.cpp
@@ -14,18 +14,29 @@ int main()
     /* Iterate over each character in the input
     This could also be done in the form (int i = 0; i < input.length(); i++)*/
 
+
+    //TODO: Input validation
     for (char c : input) 
     {
+
+
+        /* Essentially, we are checking to see if the value of char is between 48 and 57
+        If so, it must be a digit between 0 and 9, however the stdlib has a fn for this:
+        https://en.cppreference.com/w/cpp/string/byte/isdigit.html */
         if (std::isdigit(c)) 
         {
-                int digit = c - '0'; // Convert character to integer and check if its even        
-                if (digit % 2 == 0) 
-                {
-                sum += digit; // Add an even integer to sum
+
+            /* If 0 is the 48th ASCII char
+            then taking away 48 from any number between (0-9)
+            yields it's decimal value */
+            int digit = c - '0';
+            // Use of modulo operator to check for divisibility by 2
+            if (digit % 2 == 0) 
+            {
+            sum += digit; // Add an even integer to sum
             } 
         }
     }
     std::cout << "The sum of the even digits is: " << sum << "\n";
     return 0;
 }
-
